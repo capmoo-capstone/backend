756be31163fa32d9babf79a1e27ef17e295bda60
pacharapat <pcp.august@gmail.com>
2026-02-11 20:35:05 +0700

    fix: swagger

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,6 @@ import express from 'express';
 import helmet from 'helmet';
 import cors from 'cors';
 import apiV1Routes from './routes/index';
-import swaggerUi from 'swagger-ui-express';
-import swaggerDocument from '../swagger-output.json';
 import { errorHandler } from './middlewares/error';
 
 const app = express();
@@ -13,7 +11,6 @@ app.use(express.json());
 
 // Import API v1 routes
 app.use('/api/v1', apiV1Routes);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(errorHandler);
 
--- src/local.ts
+++ src/local.ts
@@ -1,6 +1,10 @@
 import app from './app';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from '../swagger-output.json';
 
 const PORT = process.env.PORT || 3000;
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running at http://localhost:${PORT}`);
