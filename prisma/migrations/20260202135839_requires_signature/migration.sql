f4832230cf5602ebeed662695882ec8ce501d3df
pacharapat <pcp.august@gmail.com>
2026-03-29 20:47:00 +0700

    refactor: db

--- prisma/migrations/20260202135839_requires_signature/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "project_submissions" ADD COLUMN     "signed_at" TIMESTAMP(3);
-
--- AlterTable
-ALTER TABLE "workflow_steps" ADD COLUMN     "requires_signature" BOOLEAN NOT NULL DEFAULT false;
