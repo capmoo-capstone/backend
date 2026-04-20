a9919e134600885926493b0a4f1d7113a2da9a5e
popo <144322354+yosita-rd@users.noreply.github.com>
2026-04-20 15:19:35 +0700

    fix: edit CONTRACT step config

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -12,7 +12,7 @@ export const WORKFLOW_STEP_ORDERS: Record<UnitResponsibleType, number[]> = {
   [UnitResponsibleType.MT500K]: [1, 2, 3, 4, 5, 6],
   [UnitResponsibleType.SELECTION]: [1, 2, 3, 4, 5, 6, 7],
   [UnitResponsibleType.EBIDDING]: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-  [UnitResponsibleType.CONTRACT]: [1, 2, 3, 4, 5, 6, 7],
+  [UnitResponsibleType.CONTRACT]: [1, 2, 3, 4],
   [UnitResponsibleType.INTERNAL]: [1, 2, 3, 4],
 };
 
