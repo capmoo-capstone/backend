ea9faffe3e9881a32a9ab03fbf86eeced0d6c72d
pacharapat <pcp.august@gmail.com>
2026-04-26 20:32:56 +0700

    fix: restructure prisma.ts

--- src/config/prisma.ts
+++ src/config/prisma.ts
@@ -3,8 +3,17 @@ import { PrismaPg } from '@prisma/adapter-pg';
 import { env } from '@prisma/config';
 import pg from 'pg';
 
-let connectionString = env('DATABASE_URL');
-const pool = new pg.Pool({ connectionString });
-const adapter = new PrismaPg(pool as any);
+const connectionString = env('DATABASE_URL');
 
+const pool = new pg.Pool({
+  connectionString,
+  idleTimeoutMillis: 30000, // close idle after 30s
+  connectionTimeoutMillis: 5000, // throw within 5s
+});
+
+pool.on('error', (err) => {
+  console.error('Unexpected error on idle client', err);
+});
+
+const adapter = new PrismaPg(pool as any);
 export const prisma = new PrismaClient({ adapter });
