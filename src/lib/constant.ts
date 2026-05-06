5e8de2095da31eadcb74a4db3d0365a74b3b02b6
pacharapat <pcp.august@gmail.com>
2026-05-06 16:47:11 +0700

    fix: contract step

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -19,7 +19,7 @@ export const WORKFLOW_STEP_ORDERS: Record<UnitResponsibleType, number[]> = {
   [UnitResponsibleType.MT500K]: [1, 2, 3, 4, 5, 6],
   [UnitResponsibleType.SELECTION]: [1, 2, 3, 4, 5, 6, 7],
   [UnitResponsibleType.EBIDDING]: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-  [UnitResponsibleType.CONTRACT]: [1, 2, 3, 4],
+  [UnitResponsibleType.CONTRACT]: [1, 2, 3, 4, 5],
   [UnitResponsibleType.INTERNAL]: [1, 2, 3, 4],
 };
 
